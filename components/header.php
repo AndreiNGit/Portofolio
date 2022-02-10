1a5e8a2e5c0d4c1945bb62e91796d982e511640a
Andrei N <nicusor.andrei118@gmail.com>
2022-02-10 17:01:13 +0200

    v1

---
+++ components/header.php
@@ -0,0 +1,10 @@
+
+<header>
+    <img src="" alt="logo">
+
+    <nav>
+        <a href="#about-me">Despre mine</a>
+        <a href="#projects">Proiectele mele</a>
+        <a href="#contact">Contact</a>
+    </nav>
+</header>

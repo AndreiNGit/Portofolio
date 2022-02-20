7a2ad48ece817d75a25c367653913469bd274704
Andrei Nicusor Andrei <nicusor.andrei118@gmail.com>
2022-02-20 22:12:30 +0200

    added desktop version

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,47 @@
+/* ------------- FUNCTIONS ------------- */
+
 function RGBA(e, alpha) { //e = jQuery element, alpha = background-opacity
     b = e.css('backgroundColor');
     e.css('backgroundColor', 'rgba' + b.slice(b.indexOf('('), ( (b.match(/,/g).length == 2) ? -1 : b.lastIndexOf(',') - b.length) ) + ', '+alpha+')');
 }
 
+function checkScroll() {
+    if($(window).scrollTop() > 50) {
+        RGBA(jQuery("header"), 0.8);
+    }
+    else if($(window).scrollTop() < 50)
+    {
+        $("#header").css("background-color", "");
+    }
+}
+
+
+/* ---------------- CODE -------------- */
 $(document).ready(function() {
-    //SETUP
-    if($(document).width() <= 768)
-        $("#header-nav").hide();
+    /* ---------------- DESKTOP SETUP -------------- */
+    if($(document).width() < 1024)
+        $("#header-nav").addClass("hidden");
     else
-        $("#header-nav").show();
+    {
+        $("#header-nav").removeClass("hidden");
+        $("#header-nav").css("margin-bottom", "0");
+    }
 
-    //MENU
+    /* ---------------- MOBILE MENU FUNCTIONALITY -------------- */
     $(".fa-bars").on("click", function() {
-        $("#header-nav").toggle(500);
-    });
-
-    $(document).scroll(function() {
-        if($(window).scrollTop() > 50) {
+        if($("#header-nav").hasClass("hidden"))
+        {
+            $("#header-nav").removeClass("hidden");
             RGBA(jQuery("header"), 0.8);
         }
-        else if($(window).scrollTop() < 50)
-        {
-            $("#header").css("background-color", "");
+        else {
+            $("#header-nav").addClass("hidden");
+            checkScroll();
         }
+    });
+
+    /* ---------------- SCROLL COLOR HEADER CHANGE ---------- */
+    $(document).scroll(function() {
+        checkScroll();
     });
 });
